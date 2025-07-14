7a7c54aef3307e113ca3cf37e7999988973a6f9f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-14 16:36:41 +0200

    add context isLoggedInStateContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { recipes } from './data/recipes';
 import { useState } from 'react';
 import CookBook from './components/CookBook/CookBook';
 import { RecipeContext } from './context/RecipeContext';
+import { TopBar } from './components/TopBar/TopBar';
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState(1);
@@ -13,16 +14,19 @@ function App() {
   );
 
   return (
-    <RecipeContext.Provider value={selectedRecipe}>
+    <>
+      <TopBar />
       <div className={styles.container}>
-        <List
-          selectedRecipe={selectedRecipe}
-          recipes={recipes}
-          onSelectRecipe={(id) => setSelectedRecipeId(id)}
-        />
-        <CookBook />
+        <RecipeContext.Provider value={selectedRecipe}>
+          <List
+            selectedRecipe={selectedRecipe}
+            recipes={recipes}
+            onSelectRecipe={(id) => setSelectedRecipeId(id)}
+          />
+          <CookBook />
+        </RecipeContext.Provider>
       </div>
-    </RecipeContext.Provider>
+    </>
   );
 }
 
